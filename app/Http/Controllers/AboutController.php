42d79bb4cf9785680b9ed682d0053f905709d3d1
GuoTingRui <azsxdc890902@gmail.com>
2021-10-25 21:23:54 +0800

    加入各頁的連結

--- app/Http/Controllers/AboutController.php
+++ app/Http/Controllers/AboutController.php
@@ -13,7 +13,7 @@ class AboutController extends Controller
      */
     public function index()
     {
-        //
+        return view('about');
     }
 
     /**
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -56,6 +56,8 @@
                             <div class="ml-12">
                                 <div class="mt-2 text-gray-600 dark:text-gray-400 text-sm">
                                     Laravel has wonderful, thorough documentation covering every aspect of the framework. Whether you are new to the framework or have previous experience with Laravel, we recommend reading all of the documentation from beginning to end.
+                                    <a href="{{route('about.index')}}">關於本站</a>
+                                    <a href="{{route('news.index')}}">最新消息</a>
                                 </div>
                             </div>
                         </div>
